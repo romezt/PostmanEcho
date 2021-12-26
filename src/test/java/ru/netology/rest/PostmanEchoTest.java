55e1461028d56d5525a0d7b5ea5593db0c9eb50a
Anton <ikkii780@mail.ru>
2021-12-26 16:35:58 +0500

    Initial commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+class MobileBankApiTestV1 {
+ @Test
+ void shouldReturnDemoAccounts() {
+  given()
+          .baseUri("http://postman-echo.com")
+          .body("some data")
+          .when()
+          .post("/post")
+          .then()
+          .statusCode(200)
+          .body("data", equalTo("some data"))
+  ;
+ }
+}
